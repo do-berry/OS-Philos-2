edc8b3faf70054429e285cd0267629f4403329ce
kvantyfikator <dominikadolik6@gmail.com>
2019-04-08 17:32:10 +0200

    additional class for table
    init of additional class for table, but I'm too bored to implement it for all cases
    next step: implement ncurses (but how should it look like??)

---
+++ Stol.cpp
@@ -0,0 +1,6 @@
+#include "Stol.h"
+
+Stol() {
+  this->lewyWidelec = false;
+  this->prawyWidelec = false;
+}
---
+++ Stol.h
@@ -0,0 +1,12 @@
+#ifndef STOL_H
+#define STOL_H
+
+class Stol {
+public:
+  bool lewyWidelec;
+  bool prawyWidelec;
+  Stol();
+  ~Stol();
+}
+
+#endif // STOL_H
